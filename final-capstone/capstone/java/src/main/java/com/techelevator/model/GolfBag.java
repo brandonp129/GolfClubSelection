786ff83bbc5b4838565d00267b167f19772871af
brandonp129 <155605027+brandonp129@users.noreply.github.com>
2025-01-16 17:29:47 -0500

    creating golfBag and golfClub models

--- final-capstone/capstone/java/src/main/java/com/techelevator/model/GolfBag.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/model/GolfBag.java
@@ -1,4 +1,51 @@
 package com.techelevator.model;
 
+import java.util.List;
+
 public class GolfBag {
+
+    private int bagId;
+
+    private int userId;
+
+    private List golfClubs;
+
+    private String name;
+
+
+    public GolfBag() {
+
+    }
+
+    public int getBagId() {
+        return bagId;
+    }
+
+    public void setBagId(int bagId) {
+        this.bagId = bagId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public List getGolfClubs() {
+        return golfClubs;
+    }
+
+    public void setGolfClubs(List golfClubs) {
+        this.golfClubs = golfClubs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- final-capstone/capstone/java/src/main/java/com/techelevator/model/GolfClubs.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/model/GolfClubs.java
@@ -1,4 +1,59 @@
 package com.techelevator.model;
 
 public class GolfClubs {
+
+    private int clubId;
+
+    private int bagId;
+
+    private int clubDistance;
+
+    private String clubType;
+
+    private String clubName;
+
+    public GolfClubs() {
+
+    }
+
+
+    public int getClubId() {
+        return clubId;
+    }
+
+    public void setClubId(int clubId) {
+        this.clubId = clubId;
+    }
+
+    public int getBagId() {
+        return bagId;
+    }
+
+    public void setBagId(int bagId) {
+        this.bagId = bagId;
+    }
+
+    public int getClubDistance() {
+        return clubDistance;
+    }
+
+    public void setClubDistance(int clubDistance) {
+        this.clubDistance = clubDistance;
+    }
+
+    public String getClubType() {
+        return clubType;
+    }
+
+    public void setClubType(String clubType) {
+        this.clubType = clubType;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
 }
