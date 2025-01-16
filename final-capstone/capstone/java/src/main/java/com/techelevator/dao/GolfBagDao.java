de61e869c8959343dd73f78c748b2b96c3de2e32
brandonp129 <155605027+brandonp129@users.noreply.github.com>
2025-01-15 23:16:04 -0500

    wrote methods in the GolfBagDao

--- final-capstone/capstone/java/src/main/java/com/techelevator/dao/GolfBagDao.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/dao/GolfBagDao.java
@@ -1,5 +1,15 @@
 package com.techelevator.dao;
 
 
+import java.util.List;
+
 public interface GolfBagDao {
+
+    //need to come back to add parameters
+    void addGolfBagToDataBaseAndUserList();
+
+    //need to come back to add parameters
+    List<Integer> getListOfUserGolfBags();
+
+    void deleteGolfBagByBagId(int golfBagId);
 }
