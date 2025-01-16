04e9c26822c7943edd41265423d0f32e0c83acac
brandonp129 <155605027+brandonp129@users.noreply.github.com>
2025-01-15 23:27:45 -0500

    starting writing the methods for the GolfClubDao

--- final-capstone/capstone/java/src/main/java/com/techelevator/dao/GolfClubDao.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/dao/GolfClubDao.java
@@ -1,4 +1,20 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.GolfClubs;
+
+import java.util.List;
+
 public interface GolfClubDao {
+
+    //need to come back to add parameters
+    void addGolfClubToDatabaseAndGolfBag();
+
+
+    //need to come back to add parameters
+    List<Integer> getListOfGolfClubsInGolfBag();
+
+    GolfClubs getGolfClubById(int golfClubId);
+
+    void deleteGolfClubById(int GolfClubId);
+
 }
